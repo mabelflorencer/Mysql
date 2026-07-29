26afc57a1aa4e31eea509f750f3a7314f1008fe2
Mabelflorence <mabelflorenceramachandran@gmail.com>
2026-07-29 13:39:30 +0530

    Add SQL script for managing user performance in mobile app challenges

---
+++ Day7HW.sql
@@ -0,0 +1,4 @@
+-- You are managing a database for a mobile app that tracks user performance in different challenges.
+INSERT INTO mobileapp (id, name, city, score, bonus, challenge) VALUES (1, 'Raj', 'Chennai', 88, 5, 'Fitness'), (2, 'Anu', 'Mumbai', 91, NULL, 'Diet'), (3, 'Ravi', 'Chennai', 78, 3, 'Fitness'), (4, 'Meena', 'Delhi', 82, NULL, 'Diet'), (5, 'Farah', 'Mumbai', 95, 4, 'Fitness'), (6, 'Kiran', 'Pune', 70, NULL, 'Yoga'), (7, 'Latha', 'Pune', 87, NULL, 'Fitness');
+SELECT * FROM `mobileapp` WHERE Score < (SELECT AVG(Score) FROM mobileapp);
+SELECT `Name` FROM `mobileapp` WHERE Challenge= (SELECT Challenge FROM mobileapp WHERE Name='Farah');
