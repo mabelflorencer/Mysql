3c42b6f41cd7ab60f23650ddebc3d921274bae46
Mabelflorence <mabelflorenceramachandran@gmail.com>
2026-07-29 14:08:16 +0530

    Add SQL script for managing bookstore inventory system

---
+++ Day8HW.sql
@@ -0,0 +1,6 @@
+-- Imagine you are helping a small bookstore manage their online inventory system. Your job is to set up the database structure for their books and authors.
+CREATE DATABASE BookStoreDB;
+ALTER TABLE books ADD published_year INT;
+ALTER TABLE books ADD published_year INT;
+TRUNCATE TABLE books;
+DROP DATABASE bookstoredb;
